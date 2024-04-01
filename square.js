7326c6ec1e89da4311326273a8d609456b79c3d0
Christoph <christoph.peter.bluem@gmail.com>
2024-04-01 11:18:31 +0200

    adds nice bounce amplification

--- square.js
+++ square.js
@@ -38,10 +38,10 @@ window.requestAnimationFrame(draw);
 
 
 const ball = {
-  x: 100,
-  y: 100,
-  vx: 5,
-  vy: 2,
+  x: canvas.width / 2,
+  y: canvas.height / 3,
+  vx: Math.floor((Math.random() * 10)) + 5,
+  vy: Math.floor((Math.random() * 5)) + 2,
   radius: 25,
   color: next_color(color_wheel[0]),
   draw() {
@@ -53,6 +53,8 @@ const ball = {
   },
 };
 
+let bounce_amplification = 0
+
 function draw() {
   ctx.fillStyle = "rgb(0 0 0 / 30%)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -62,22 +64,29 @@ function draw() {
   ball.vy *= 0.99;
   ball.vy += 0.25;
 
-
+  // bo'om top bounce
   if (
     ball.y + ball.vy > canvas.height - ball.radius ||
     ball.y + ball.vy < ball.radius
   ) {
-    ball.vy = -ball.vy;
+    bounce_amplification += .005
+    let bounce_sign = ball.y + ball.vy > canvas.height - ball.radius ? -1 : +1
+    ball.y = ball.y + ball.vy > canvas.height - ball.radius ? canvas.height - ball.radius : ball.y + ball.vy < ball.radius ? ball.radius : ball.y + ball.vy
+    ball.vy = -ball.vy + bounce_sign * bounce_amplification * 20;
     ball.color = next_color(ball.color) 
     ball.radius += 1
     const temp_audio = pop.cloneNode()
     temp_audio.play()
   }
+  // side bounce
   if (
     ball.x + ball.vx > canvas.width - ball.radius ||
     ball.x + ball.vx < ball.radius
   ) {
-    ball.vx = -ball.vx;
+    bounce_amplification += .005
+    ball.x = ball.x + ball.vx > canvas.width - ball.radius ? canvas.clientWidth - ball.radius : ball.x + ball.vx < ball.radius ? ball.radius : ball.x + ball.vx
+    let bounce_sign = ball.x + ball.vx > canvas.clientWidth - ball.radius? -1 : +1
+    ball.vx = -ball.vx + bounce_sign * bounce_amplification;
     ball.color = next_color(ball.color) 
     ball.radius += 1
     const temp_audio = pop.cloneNode()
